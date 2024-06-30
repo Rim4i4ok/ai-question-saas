470661f0055baa340d730606d9b29c4334125e9a
Rim4i4ok <rim4i4ok@gmail.com>
2024-06-30 23:19:10 +0200

    showing user button

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
-  return <Button variant="ghost">Hello Dashboard world!</Button>;
+  return (
+    <div>
+      <p>Dashboard Page (Protected)</p>
+      <UserButton />
+    </div>
+  );
 }
