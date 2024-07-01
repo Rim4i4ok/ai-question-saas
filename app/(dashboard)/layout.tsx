110384393e0d9c839c734d85c9fde8774a452e17
Rim4i4ok <rim4i4ok@gmail.com>
2024-07-01 21:35:26 +0200

    adding layout for dashboard

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,12 @@
+function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="relative h-full">
+      <div className="z-[80] hidden h-full bg-gray-900 md:fixed md:inset-y-0 md:flex md:w-72 md:flex-col">
+        <div>Hello Sidebar</div>
+      </div>
+      <main className="md:pl-72">{children}</main>
+    </div>
+  );
+}
+
+export default DashboardLayout;
