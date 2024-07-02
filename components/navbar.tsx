3e6e5270edb367b68973b4a4738f8ce7764f4a02
Rim4i4ok <rim4i4ok@gmail.com>
2024-07-02 23:37:08 +0200

    adding mobile side bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,11 @@
-import { Menu } from "lucide-react";
-import { Button } from "./ui/button";
 import { UserButton } from "@clerk/nextjs";
+
+import MobileSideBar from "./mobile-sidebar";
 
 function Navbar() {
   return (
     <div className="flex items-center p-4">
-      <Button variant="ghost" size="icon" className="md:hidden">
-        <Menu />
-      </Button>
+      <MobileSideBar />
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>
