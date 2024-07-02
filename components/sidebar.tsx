9174859c7f79b97fa51aa9646d5c248b06aa817d
Rim4i4ok <rim4i4ok@gmail.com>
2024-07-02 23:28:35 +0200

    adding sidebar links

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,7 +1,65 @@
 "use client";
 
+import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
+import {
+  CodeIcon,
+  ImageIcon,
+  LayoutDashboard,
+  MessageSquare,
+  MusicIcon,
+  SettingsIcon,
+  VideoIcon,
+} from "lucide-react";
+
+import { cn } from "@/lib/utils";
+
+const montserrat = Montserrat({ weight: "600", subsets: ["cyrillic"] });
+
+const routes = [
+  {
+    label: "Dashboard",
+    icon: LayoutDashboard,
+    href: "/dashboard",
+    color: "text-sky-500",
+  },
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    href: "/conversation",
+    color: "text-violet-500",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    href: "/image",
+    color: "text-pink-700",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    href: "/video",
+    color: "text-yellow-700",
+  },
+  {
+    label: "Music Generation",
+    icon: MusicIcon,
+    href: "/music",
+    color: "text-green-700",
+  },
+  {
+    label: "Code Generation",
+    icon: CodeIcon,
+    href: "/code",
+    color: "text-emerald-700",
+  },
+  {
+    label: "Settings",
+    icon: SettingsIcon,
+    href: "/settings",
+  },
+];
 
 function Sidebar() {
   return (
@@ -11,8 +69,24 @@ function Sidebar() {
           <div className="relative mr-4 h-8 w-8">
             <Image fill alt="Logo" src="/logo.png" />
           </div>
-          <h1 className="text-2xl font-bold">Clever boy</h1>
+          <h1 className={cn("text-2xl font-bold", montserrat.className)}>
+            Clever boy
+          </h1>
         </Link>
+        <div className="space-y-1">
+          {routes.map((route) => (
+            <Link
+              href={route.href}
+              key={route.href}
+              className="group flex w-full cursor-pointer justify-start rounded-lg p-3 text-sm font-medium transition hover:bg-white/10 hover:text-white"
+            >
+              <div className="flex flex-1 items-center">
+                <route.icon className={cn("mr-3 h-5 w-5", route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
