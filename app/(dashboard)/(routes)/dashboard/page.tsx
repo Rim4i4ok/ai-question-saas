6ad315e42bc13c7c7e558376f3e5180ddc922057
Rim4i4ok <rim4i4ok@gmail.com>
2024-07-03 00:02:56 +0200

    adding dashboard page links

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,87 @@
+"use client";
+
+import {
+  ArrowRight,
+  CodeIcon,
+  ImageIcon,
+  MessageSquare,
+  MusicIcon,
+  VideoIcon,
+} from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    color: "text-pink-500",
+    bgColor: "bg-pink-500/10",
+    href: "/image",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    color: "text-yellow-500",
+    bgColor: "bg-yellow-500/10",
+    href: "/video",
+  },
+  {
+    label: "Music Generation",
+    icon: MusicIcon,
+    color: "text-green-500",
+    bgColor: "bg-green-500/10",
+    href: "/music",
+  },
+  {
+    label: "Code Generation",
+    icon: CodeIcon,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/code",
+  },
+];
+
 export default function DashboardPage() {
+  const router = useRouter();
+
   return (
     <div>
-      <p>Dashboard Page (Protected)</p>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-center text-2xl font-bold md:text-4xl">
+          Explore the power of AI
+        </h2>
+        <h2 className="text-center text-sm font-light text-muted-foreground md:text-lg">
+          Chat with the smartest AI - Experience the power of AI
+        </h2>
+      </div>
+      <div className="space-y-4 px-4 md:px-20 lg:px-32">
+        {tools.map((tool) => (
+          <Card
+            key={tool.href}
+            className="flex cursor-pointer items-center justify-between border-black/5 p-4 transition hover:shadow-md"
+            onClick={() => router.push(tool.href)}
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("w-fit rounded-md p-2", tool.bgColor)}>
+                <tool.icon className={cn("h-8 w-8", tool.color)} />
+              </div>
+              <div className="font-semibold">{tool.label}</div>
+            </div>
+            <ArrowRight className="h-5 w-5" />
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
