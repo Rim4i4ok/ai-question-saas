e63dc514ecac689988b2092a70c1110e9840e8a8
Rim4i4ok <rim4i4ok@gmail.com>
2024-07-10 23:09:54 +0200

    adding constants to images

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -4,34 +4,28 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
 import { ImageIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
-import OpenAI from "openai";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
-import BotAvatar from "@/components/bot-avatar";
 import Empty from "@/components/empty";
 import Heading from "@/components/heading";
 import Loader from "@/components/loader";
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import UserAvatar from "@/components/user-avatar";
-import { cn } from "@/lib/utils";
 
 import { TFormSchema, formSchema } from "./validation";
 
-type Message =
-  | OpenAI.Chat.ChatCompletionUserMessageParam
-  | OpenAI.Chat.ChatCompletionAssistantMessageParam;
-
 function ImagePage() {
   const router = useRouter();
-  const [messages, setMessages] = useState<Message[]>([]);
+  const [images, setImages] = useState<string[]>([]);
 
   const form = useForm<TFormSchema>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       prompt: "",
+      amount: "1",
+      resolution: "512x512",
     },
   });
 
@@ -39,19 +33,12 @@ function ImagePage() {
 
   const onSubmitHandler = async (values: TFormSchema) => {
     try {
-      const userMessage: OpenAI.Chat.ChatCompletionMessageParam = {
-        role: "user",
-        content: values.prompt,
-      };
+      setImages([]);
 
-      const newMessages = [...messages, userMessage];
+      const response = await axios.post("/api/image", values);
+      const urls = response.data.map((image: { url: string }) => image.url);
 
-      const response = await axios.post("/api/conversation", {
-        messages: newMessages,
-      });
-
-      setMessages((current) => [...current, userMessage, response.data]);
-
+      setImages(urls);
       form.reset();
     } catch (error) {
       // TODO: Open Pro Modal
@@ -85,7 +72,7 @@ function ImagePage() {
                       <Input
                         {...field}
                         disabled={isLoading}
-                        placeholder="How many episodes at Star Wars saga?"
+                        placeholder="A picture of a duck in the office"
                         className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
                       />
                     </FormControl>
@@ -103,29 +90,14 @@ function ImagePage() {
         </div>
         <div className="mt-4 space-y-4">
           {isLoading && (
-            <div className="flex w-full items-center justify-center rounded-lg bg-muted p-8">
+            <div className="p-20">
               <Loader />
             </div>
           )}
-          {messages.length === 0 && !isLoading && (
-            <Empty label="No conversation started" />
+          {images.length === 0 && !isLoading && (
+            <Empty label="No images generated" />
           )}
-          <div className="flex flex-col-reverse gap-y-4">
-            {messages.map((message) => (
-              <div
-                key={message.content?.toString()}
-                className={cn(
-                  "flex w-full items-start gap-x-8 rounded-lg bg-slate-200 p-8",
-                  message.role === "user"
-                    ? "border border-black/10 bg-white"
-                    : "bg-muted",
-                )}
-              >
-                {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
-                <p className="text-sm">{message.content?.toString()}</p>
-              </div>
-            ))}
-          </div>
+          <div>Images will be rendered here</div>
         </div>
       </div>
     </div>
