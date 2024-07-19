599773136966036fe5b020cccb73d25af3629b35
Rim4i4ok <rim4i4ok@gmail.com>
2024-07-19 14:02:11 +0200

    adding pro modal content

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,8 +1,68 @@
 "use client";
 
-import { useProModal } from "@/hooks/use-pro-modal";
+import {
+  Check,
+  CodeIcon,
+  ImageIcon,
+  MessageSquare,
+  MusicIcon,
+  VideoIcon,
+  Zap,
+} from "lucide-react";
 
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
+import { useProModal } from "@/hooks/use-pro-modal";
+import { cn } from "@/lib/utils";
+
+import { Badge } from "./ui/badge";
+import { Button } from "./ui/button";
+import { Card } from "./ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    color: "text-pink-500",
+    bgColor: "bg-pink-500/10",
+    href: "/image",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    color: "text-yellow-500",
+    bgColor: "bg-yellow-500/10",
+    href: "/video",
+  },
+  {
+    label: "Music Generation",
+    icon: MusicIcon,
+    color: "text-green-500",
+    bgColor: "bg-green-500/10",
+    href: "/music",
+  },
+  {
+    label: "Code Generation",
+    icon: CodeIcon,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/code",
+  },
+];
 
 export const ProModal = () => {
   const proModal = useProModal();
@@ -12,9 +72,35 @@ export const ProModal = () => {
       <DialogContent>
         <DialogHeader>
           <DialogTitle className="flex flex-col items-center justify-center gap-y-4 pb-2">
-            Upgrade to Premium
+            <div className="flex items-center gap-x-2 py-1 font-bold">
+              Upgrade to Premium
+              <Badge variant="premium" className="py-1 text-sm uppercase">
+                pro
+              </Badge>
+            </div>
           </DialogTitle>
+          <DialogDescription className="space-y-2 pt-2 text-center font-medium text-zinc-900">
+            {tools.map((tool) => (
+              <Card
+                key={tool.label}
+                className="flex items-center justify-between border-black/5 p-3"
+              >
+                <div className="flex items-center gap-x-4">
+                  <div className={cn("w-fit rounded-md p-2", tool.bgColor)}>
+                    <tool.icon className={cn("h-6 w-6", tool.color)} />
+                  </div>
+                  <div className="text-sm font-semibold">{tool.label}</div>
+                </div>
+                <Check className="h-5 w-5 text-primary" />
+              </Card>
+            ))}
+          </DialogDescription>
         </DialogHeader>
+        <DialogFooter>
+          <Button size="lg" variant="premium" className="w-full">
+            Upgrade <Zap className="ml-2 h-4 w-4 fill-white" />
+          </Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
